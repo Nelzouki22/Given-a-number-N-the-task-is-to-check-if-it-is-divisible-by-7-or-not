d7480a709a5d95fbbb2c948a6a28be00802c19b1
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-05-26 21:51:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int N))
+        {
+            if (IsDivisibleBy7(N))
+            {
+                Console.WriteLine($"{N} is divisible by 7.");
+            }
+            else
+            {
+                Console.WriteLine($"{N} is not divisible by 7.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    static bool IsDivisibleBy7(int N)
+    {
+        return N % 7 == 0;
+    }
+}
+
